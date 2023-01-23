02cfd01734220e4c9dfd1c12101154bf29ba73d6
omrilotan <omrilotan@hotmail.com>
2023-01-23 14:51:41 +0000

    Change form destination

--- docs/assets/scripts.js
+++ docs/assets/scripts.js
@@ -13,14 +13,20 @@
 			var data = serialise(form);
 
 			form.setAttribute('disabled', 'disabled');
+			data.recipient = 'ilanlahav88@gmail.com';
 
-			fetch('https://hooks.zapier.com/hooks/catch/616786/oh8pobp/silent/', {
-				method: 'post',
-				mode: 'cors',
-				cache: 'no-cache',
-				redirect: 'follow',
-				referrerPolicy: 'no-referrer',
-				body: JSON.stringify(data)
+			fetch('https://message.omrilotan.com', {
+					headers: {
+						'Content-Type': 'application/json; charset=utf-8',
+						authorization: 'F7298EBA-24B7-4231-BE79-82DF2CD41DCE'
+					},
+					method: 'POST',
+					mode: 'cors',
+					cache: 'no-cache',
+					credentials: 'same-origin',
+					redirect: 'follow',
+					referrerPolicy: 'no-referrer',
+					body: JSON.stringify(data)
 			}).then(function (response) {
 				if (response.ok) {
 					var thankyou = document.getElementById('thankyou');
@@ -41,24 +47,24 @@
 			alert(error.message);
 		}
 	}
+
+	function serialise(form) {
+		return [].reduce.call(
+			form,
+			function(accumulator, item) {
+				if (item.hasAttribute('required') && !item.value) {
+					throw new Error('נא למלא ' + item.name);
+				}
+
+				if (item.value) {
+					accumulator[item.name] = item.value.trim();
+				}
+				return accumulator;
+			},
+			{}
+		);
+	}
 })();
-
-function serialise(form) {
-	return [].reduce.call(
-		form,
-		function(accumulator, item) {
-			if (item.hasAttribute('required') && !item.value) {
-				throw new Error('נא למלא ' + item.name);
-			}
-
-			if (item.value) {
-				accumulator[item.name] = item.value.trim();
-			}
-			return accumulator;
-		},
-		{}
-	);
-}
 
 (function() {
 	var nav = document.getElementsByTagName('nav')[0];
